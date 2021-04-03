8b4f3eb582f63d6f88789082493e284eecad7264
sandeepjhaVC <heyyoumailmeat@gmail.com>
2021-04-03 17:41:06 +0530

    VanishingPigs

--- Bird.js
+++ Bird.js
@@ -1,6 +1,7 @@
 class Bird extends BaseClass {
   constructor(x,y){
     super(x,y,50,50);
+    Matter.Body.setMass(this.body, this.body.mass*3);
     this.image = loadImage("sprites/bird.png");
   }
 
